8e316c97d197cd5f1824136c3fad7f7cb25121e4
Sadia Batool <sadia.batool@arcanainfo.com>
2026-01-09 15:45:14 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Result:", multiply(4, 5))
+
---
+++ test_app.py
@@ -0,0 +1,5 @@
+from app import multiply
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+
